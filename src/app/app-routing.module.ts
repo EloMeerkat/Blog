d9cdb57c3d2413d28e9fc0766d40e43c95dc6d79
Axel HEINE <axheine@gmail.com>
2018-05-03 19:08:28 +0200

    Fix: searchbar css transitons and route param name

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,7 +11,7 @@ const routes: Routes = [
   { path: 'homepage', component: HomepageComponent },
   { path: 'articles', component: ArticleComponent },
   { path: 'detail/:id', component: ArticleDetailComponent },
-  { path: 'articles/:type', component: ArticleComponent }
+  { path: 'articles/:category', component: ArticleComponent }
 ];
 
 @NgModule({
